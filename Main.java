4161d92de7e9eba0ecbc5c509240caedcef6d12b
Caleb Lagge <caleb.i.lagge-1@ou.edu>
2019-02-26 19:07:59 -0600

    Some document changes

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ public class Main
 {
 	public static void main(String[] args) throws IOException 
 	{
-		//String StID = "OKCE";
-		String StID = "NRMN";
+		String StID = "OKCE";
+		//String StID = "NRMN";
 		MesoInherit mesoinherit = new MesoInherit(new MesoStation(StID));
 		
 		System.out.println("Ascii Ceiling is: " + mesoinherit.calAverage()[0]
 			+ ", Ascii Floor is: " + mesoinherit.calAverage()[1]
-				+ ", Ascii Average is: " + mesoinherit.calAverage()[2]);
-					//+ ", and Letter Avg is: " + mesoinherit.letterAverage());
+				+ ", Ascii Average is: " + mesoinherit.calAverage()[2]
+					+ ", and Letter Avg is: " + mesoinherit.letterAverage());
 	}
 }
--- MesoInherit.java
+++ MesoInherit.java
@@ -41,7 +41,8 @@ public class MesoInherit extends MesoAbstract
 			//assign decide whether the average should be rounded up or down
 			if (i == 0)
 			{
-				//if the average has a decimal of 0.5 or greater round up
+				characterAverages[0] = (int)Math.ceil(average);
+				/*//if the average has a decimal of 0.5 or greater round up
 				if ((Math.ceil(average)-average) <= 0.5)
 				{
 					 characterAverages[0] = (int)Math.ceil(average);
@@ -50,17 +51,28 @@ public class MesoInherit extends MesoAbstract
 				else 
 				{
 					characterAverages[0] = (int)Math.floor(average);
-				}
+				}*/
 			}
-			//assign the ceiling to hte second index
+			//assign the ceiling to the second index
 			if (i == 1)
 			{
-				characterAverages[1] = (int)Math.ceil(average);
+				characterAverages[1] = (int)Math.floor(average);
+				//characterAverages[1] = (int)Math.ceil(average);
 			}
 			//assign the floor to the third index
 			if (i == 2)
 			{
-				characterAverages[2] = (int)Math.floor(average);
+				//characterAverages[2] = (int)Math.floor(average);
+				//if the average has a decimal of 0.5 or greater round up
+				if ((Math.ceil(average)-average) <= 0.5)
+				{
+					 characterAverages[2] = (int)Math.ceil(average);
+				}
+				//if the average has a decimal less than 0.5 round down
+				else 
+				{
+					characterAverages[2] = (int)Math.floor(average);
+				}
 			}
 		
 		}
@@ -72,7 +84,43 @@ public class MesoInherit extends MesoAbstract
 	@Override
 	public char letterAverage() {
 		// TODO Auto-generated method stub
-		return 0;
+		//a string to store the string that is being parsed
+				String ID = mesoString;
+				//the each index of the characterID array list represents a character in the ID string
+				int characterAverage = 0;
+				double[] asciiCharacterValue = new double[4];
+				//create a for loop to find the ascii value of each character of the ID string and add it
+				//to the asciiValue array
+				for(int index = 0; index < asciiCharacterValue.length; ++index)
+				{
+					asciiCharacterValue[index] = (int)ID.charAt(index);
+				}
+						
+				//create a variable to hold the total value
+				double totalValue = 0;
+				//calculate the average of the values
+				for (int count = 0; count < asciiCharacterValue.length; ++count)
+				{
+					totalValue = totalValue + asciiCharacterValue[count];
+				}
+						
+				//compute the average
+				double average = totalValue / asciiCharacterValue.length;
+				
+				//if the average has a decimal of 0.5 or greater round up
+				if ((Math.ceil(average)-average) <= 0.5)
+				{
+					 characterAverage = (int)Math.ceil(average);
+				}
+				//if the average has a decimal less than 0.5 round down
+				else 
+				{
+					characterAverage = (int)Math.floor(average);
+				}
+						
+				//make the average into a character
+				char averageCharacter = (char)characterAverage;
+				return averageCharacter;
 	}	
 }
 
